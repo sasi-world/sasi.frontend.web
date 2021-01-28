8ae2d0609d4668c1132758a78e9b6608231564bb
AmoDinho <u14284783@tuks.co.za>
2021-01-28 21:49:35 +0200

    Removed the stuff

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,31 @@
-import App from '../components/App'
-import InfoBox from '../components/InfoBox'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
+import App from "../components/App";
+import InfoBox from "../components/InfoBox";
+import Header from "../components/Header";
+import Submit from "../components/Submit";
 import PostList, {
   ALL_POSTS_QUERY,
   allPostsQueryVars,
-} from '../components/PostList'
-import { initializeApollo, addApolloState } from '../lib/apolloClient'
+} from "../components/PostList";
+import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => (
   <App>
-    <Header />
-    <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-    <Submit />
-    <PostList />
+    <h1 className="text-green-500">WELCOME TO SASSY WORLD</h1>
   </App>
-)
+);
 
 export async function getStaticProps() {
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo();
 
   await apolloClient.query({
     query: ALL_POSTS_QUERY,
     variables: allPostsQueryVars,
-  })
+  });
 
   return addApolloState(apolloClient, {
     props: {},
     revalidate: 1,
-  })
+  });
 }
 
-export default IndexPage
+export default IndexPage;
