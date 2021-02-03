d450e9eb057634a9d99b48b561d02fed07bb6e6a
AmoDinho <u14284783@tuks.co.za>
2021-02-03 21:40:56 +0200

    addded proptypes and nav contettn

--- components/ui/navigation/NavBar.js
+++ components/ui/navigation/NavBar.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <header className="border-top bg-transparent flex items-center justify-between flex-wrap p-6">
+      <div className="flex items-center flex-no-shrink text">
+        <h1 className="text-red font-bold font-display lg:text-3xl s:text-xl">
+          <Link
+            to="/"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            Sasi{" "}
+          </Link>
+        </h1>
+      </div>
+      {/* <span>
+        <a
+          className="flex flex-row"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Fullstack-Serverless-GraphQL/fullstack-serverless-graphql-docs"
+        >
+          <img src={Github} alt="github icon" />
+        </a>
+      </span> */}
+    </header>
+  );
 };
 
 export default NavBar;
