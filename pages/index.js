91a1fb10e857ee77079aa73f72d5e938f025f31f
AmoDinho <u14284783@tuks.co.za>
2021-02-03 21:16:05 +0200

    background is working

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
 import App from "../components/App";
-import { initializeApollo, addApolloState } from "../lib/apolloClient";
+// import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => (
   <App>
     <h1 className="text-green-500">WELCOME TO SASSY WORLD</h1>
+
+    <div className="p-4 bg-gradient-to-r from-green-400 to-blue-400"></div>
   </App>
 );
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo();
+// export async function getStaticProps() {
+//   const apolloClient = initializeApollo();
 
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  });
+//   await apolloClient.query({
+//     query: ALL_POSTS_QUERY,
+//     variables: allPostsQueryVars,
+//   });
 
-  return addApolloState(apolloClient, {
-    props: {},
-    revalidate: 1,
-  });
-}
+//   return addApolloState(apolloClient, {
+//     props: {},
+//     revalidate: 1,
+//   });
+// }
 
 export default IndexPage;
