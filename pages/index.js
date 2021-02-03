1b5ed5c46f2050549e8d87b5d97e81434aa5a02e
Brandon <brandon.dlamini95@gmail.com>
2021-02-03 22:15:18 +0200

    imported Green Button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import App from "../components/App";
+import {GreenButton} from "./components/ui/buttons"
 // import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => (
   <App>
     <h1 className="text-green-500 font-display p-5">WELCOME TO SASSY WORLD</h1>
+    <GreenButton>Im a green Button</GreenButton>
     <p className="text-green-700 font-display p-5">
       We are busy making a PLACE to get to multirracial stock images :D
     </p>
