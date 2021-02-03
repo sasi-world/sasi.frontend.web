c7b991f62bf78186a150b4e610df91e5e0ec4a38
Brandon <brandon.dlamini95@gmail.com>
2021-02-03 22:10:59 +0200

    just created green button

---
+++ components/ui/buttons/GreenButton.js
@@ -0,0 +1,11 @@
+import React, { Children } from 'react';
+
+const GreenButton = (children) => {
+    
+    
+    return(
+        <button>{children}</button>
+    );    
+}
+
+export default GreenButton;
