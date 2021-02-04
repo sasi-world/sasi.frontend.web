fb6e2340c2567338445162ec8e9b7d55956f04e7
Brandon <brandon.dlamini95@gmail.com>
2021-02-04 22:16:48 +0200

    added border color

--- components/ui/buttons/flatButtons/FlatGreenButton.js
+++ components/ui/buttons/flatButtons/FlatGreenButton.js
@@ -12,7 +12,7 @@ const FlatGreenButton = ({
     
     
     return(
-        <button className ={`${className} uppercase cursor-pointer rounded-lg shadow-md py-2 text-flat-600 bg-transparent  border-2 border-solid  hover:bg-flat-600 hover:text-white active:bg-flat-600`}
+        <button className ={`${className} uppercase cursor-pointer rounded-lg shadow-md py-2 text-flat-600 bg-transparent  border-2 border-solid  hover:bg-flat-600 hover:text-white hover:border hover:border-flat-600 active:bg-flat-600`}
         onClick ={onClick}
         disabled ={disabled}
         {...props}
