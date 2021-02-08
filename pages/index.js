2de5d35e9d22a24861a01e9da48b909997d57f5a
Brandon <brandon.dlamini95@gmail.com>
2021-02-08 22:41:21 +0200

    added map function to map out icons

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import {SocialButton} from "../components/ui/buttons";
 import {BodyOne} from "../components/ui/Typography";
 import {HeadingOne} from "../components/ui/Typography";
 import {HeadingTwo} from "../components/ui/Typography";
+import {socialLinks} from "../constants"
 
 
 // import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => {
-  
+
   return(
     <App>
       
@@ -16,10 +17,9 @@ const IndexPage = () => {
       <BodyOne className = " p-4 text-center">I'm baby readymade thundercats offal, semiotics swag subway tile actually jianbing. Enamel pin pok pok cliche live-edge disrupt put a bird on it tote bag master cleanse subway tile heirloom cred kombucha seitan kogi lo-fi. Raclette wolf celiac man braid, migas neutra taxidermy twee chartreuse adaptogen kombucha put a bird on it. Vape chicharrones pok pok try-hard humblebrag mumblecore. Waistcoat wayfarers tofu, pork belly viral edison bulb raw denim selvage cred gochujang mixtape cardigan distillery etsy leggings. Mixtape lomo readymade pok pok gluten-free crucifix. Etsy blog lo-fi everyday carry tbh.</BodyOne>  
   
     <div className ="h-48 p-3 flex flex-wrap content-start text-center">
-      <SocialButton>Fb</SocialButton>
-      <SocialButton>SC</SocialButton>
-      <SocialButton>SC</SocialButton>
-      <SocialButton>SC</SocialButton>
+      {socialLinks.map((link, index) => (
+        <SocialButton key={index} >{link.icon} </SocialButton>
+      ))}
     </div>
      </App>
   )
