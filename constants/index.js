82caa8dd05d42662cd4af09d2bacc5ea0475247d
Brandon <brandon.dlamini95@gmail.com>
2021-02-08 22:36:38 +0200

    added social Links

---
+++ constants/index.js
@@ -0,0 +1,15 @@
+export const socialLinks = [
+    {
+        url: "https://www.pinterest.co.uk/real_sasi/_saved/",
+        icon: "PT",
+    },
+    {
+        url: "https://twitter.com/sasi_world ",
+        icon: "TW",
+    },
+    {
+        url: "https://www.instagram.com/sasi.world/",
+        icon: "IG",
+    },
+
+]
