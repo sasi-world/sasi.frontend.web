22e7e76757337aef4b129081bd87360ba3175529
Brandon <brandon.dlamini95@gmail.com>
2021-02-08 22:32:38 +0200

    added Main page content

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,29 @@
 import App from "../components/App";
-import {GreenButton} from "../components/ui/buttons";
-import {OrangeButton} from"../components/ui/buttons";
 import {SocialButton} from "../components/ui/buttons";
-import {FlatGreenButton} from "../components/ui/buttons";
 import {BodyOne} from "../components/ui/Typography";
 import {HeadingOne} from "../components/ui/Typography";
 import {HeadingTwo} from "../components/ui/Typography";
 
+
 // import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
-const IndexPage = () => (
-  <App>
-    <h1 className="text-green-500 font-display p-5">WELCOME TO SASSY WORLD</h1>
-    <GreenButton>Green Button</GreenButton>
-    <FlatGreenButton>Flat G Button</FlatGreenButton>
-    <OrangeButton>Orange Button</OrangeButton>
-    <HeadingOne>Heading One</HeadingOne>
-    <HeadingTwo>Heading Two</HeadingTwo>
-    <BodyOne>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus mattis</BodyOne>
-    <SocialButton>SC</SocialButton>
-
-
-    <p className="text-green-700 font-display p-5">
-      We are busy making a PLACE to get to multirracial stock images :D
-    </p>
-  </App>
-);
+const IndexPage = () => {
+  
+  return(
+    <App>
+      
+      <HeadingOne className = "text-center flex justify-center my-2 "> Welcome to SASI World</HeadingOne>
+      <BodyOne className = " p-4 text-center">I'm baby readymade thundercats offal, semiotics swag subway tile actually jianbing. Enamel pin pok pok cliche live-edge disrupt put a bird on it tote bag master cleanse subway tile heirloom cred kombucha seitan kogi lo-fi. Raclette wolf celiac man braid, migas neutra taxidermy twee chartreuse adaptogen kombucha put a bird on it. Vape chicharrones pok pok try-hard humblebrag mumblecore. Waistcoat wayfarers tofu, pork belly viral edison bulb raw denim selvage cred gochujang mixtape cardigan distillery etsy leggings. Mixtape lomo readymade pok pok gluten-free crucifix. Etsy blog lo-fi everyday carry tbh.</BodyOne>  
+  
+    <div className ="h-48 p-3 flex flex-wrap content-start text-center">
+      <SocialButton>Fb</SocialButton>
+      <SocialButton>SC</SocialButton>
+      <SocialButton>SC</SocialButton>
+      <SocialButton>SC</SocialButton>
+    </div>
+     </App>
+  )
+};
 
 // export async function getStaticProps() {
 //   const apolloClient = initializeApollo();
