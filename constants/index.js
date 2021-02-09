4db0f6d8fb453b989f9bf52ab637fa5942bfc0ad
Brandon <brandon.dlamini95@gmail.com>
2021-02-09 12:23:34 +0200

    added Icon Links

--- constants/index.js
+++ constants/index.js
@@ -1,15 +1,23 @@
-export const socialLinks = [
-    {
-        url: "https://www.pinterest.co.uk/real_sasi/_saved/",
-        icon: "PT",
-    },
-    {
-        url: "https://twitter.com/sasi_world ",
-        icon: "TW",
-    },
-    {
-        url: "https://www.instagram.com/sasi.world/",
-        icon: "IG",
-    },
+import {
+  faFacebook,
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-]
+export const socialLinks = [
+  {
+    url: "https://www.pinterest.co.uk/real_sasi/_saved/",
+    icon: <FontAwesomeIcon icon={faFacebookF} />,
+  },
+  {
+    url: "https://twitter.com/sasi_world ",
+    icon: <FontAwesomeIcon icon={faTwitter} />,
+  },
+  {
+    url: "https://www.instagram.com/sasi.world/",
+    icon: <FontAwesomeIcon icon={faInstagram} />,
+  },
+];
