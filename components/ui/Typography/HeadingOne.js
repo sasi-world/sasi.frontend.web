dc63b9bf11a0f0208a7c3fc09f1e838bf872d629
Brandon <brandon.dlamini95@gmail.com>
2021-02-09 11:19:22 +0200

    Added Gradient to Heading

--- components/ui/Typography/HeadingOne.js
+++ components/ui/Typography/HeadingOne.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const HeadingOne = ({className, children}) => {
-
-    return(
-        <h1 className = {`${className} font-display text-4xl text-white text-bold`}>{children}</h1>
-    );
+const HeadingOne = ({ className, children }) => {
+  return (
+    <h1 className={`${className} font-display text-4xl  text-bold`}>
+      {children}
+    </h1>
+  );
 };
 
 HeadingOne.propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.oneOfType([PropTypes.string, PropTypes.array])
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
 };
 
 HeadingOne.defaultProps = {
-    className:"",
+  className: "",
 };
 
 export default HeadingOne;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,38 @@
 import App from "../components/App";
-import {SocialButton} from "../components/ui/buttons";
-import {BodyOne} from "../components/ui/Typography";
-import {HeadingOne} from "../components/ui/Typography";
-import {HeadingTwo} from "../components/ui/Typography";
-import {socialLinks} from "../constants"
-
+import { SocialButton } from "../components/ui/buttons";
+import { BodyOne } from "../components/ui/Typography";
+import { HeadingOne } from "../components/ui/Typography";
+import { HeadingTwo } from "../components/ui/Typography";
+import { socialLinks } from "../constants";
 
 // import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => {
-
-  return(
+  return (
     <App>
-      
-      <HeadingOne className = "text-center flex justify-center my-2 "> Welcome to SASI World</HeadingOne>
-      <BodyOne className = " p-4 text-center">I'm baby readymade thundercats offal, semiotics swag subway tile actually jianbing. Enamel pin pok pok cliche live-edge disrupt put a bird on it tote bag master cleanse subway tile heirloom cred kombucha seitan kogi lo-fi. Raclette wolf celiac man braid, migas neutra taxidermy twee chartreuse adaptogen kombucha put a bird on it. Vape chicharrones pok pok try-hard humblebrag mumblecore. Waistcoat wayfarers tofu, pork belly viral edison bulb raw denim selvage cred gochujang mixtape cardigan distillery etsy leggings. Mixtape lomo readymade pok pok gluten-free crucifix. Etsy blog lo-fi everyday carry tbh.</BodyOne>  
-  
-    <div className =" p-3 flex justify-center">
-      {socialLinks.map((link, index) => (
-        <SocialButton key={index} >{link.icon} </SocialButton>
-      ))}
-    </div>
-     </App>
-  )
+      <HeadingOne className="text-center bg-clip-text text-transparent bg-gradient-to-r from-gradient-three-colorB to-gradient-three-colorA flex justify-center my-2 ">
+        {" "}
+        Welcome to SASI World
+      </HeadingOne>
+      <BodyOne className=" p-4 text-center">
+        I'm baby readymade thundercats offal, semiotics swag subway tile
+        actually jianbing. Enamel pin pok pok cliche live-edge disrupt put a
+        bird on it tote bag master cleanse subway tile heirloom cred kombucha
+        seitan kogi lo-fi. Raclette wolf celiac man braid, migas neutra
+        taxidermy twee chartreuse adaptogen kombucha put a bird on it. Vape
+        chicharrones pok pok try-hard humblebrag mumblecore. Waistcoat wayfarers
+        tofu, pork belly viral edison bulb raw denim selvage cred gochujang
+        mixtape cardigan distillery etsy leggings. Mixtape lomo readymade pok
+        pok gluten-free crucifix. Etsy blog lo-fi everyday carry tbh.
+      </BodyOne>
+
+      <div className=" p-3 flex justify-center">
+        {socialLinks.map((link, index) => (
+          <SocialButton key={index}>{link.icon} </SocialButton>
+        ))}
+      </div>
+    </App>
+  );
 };
 
 // export async function getStaticProps() {
