7a04f158bcd4afcc62832a898fbddf17674990f6
Brandon <brandon.dlamini95@gmail.com>
2021-02-12 00:03:53 +0200

    added text in homepage

--- pages/index.js
+++ pages/index.js
@@ -9,25 +9,33 @@ import { socialLinks } from "../constants";
 const IndexPage = () => {
   return (
     <App>
-      <HeadingOne className="text-center bg-clip-text my text-transparent bg-gradient-to-r from-gradient-three-colorB to-gradient-three-colorA flex justify-center my-2 ">
+      <HeadingOne className="text-center font-medium bg-clip-text my text-transparent bg-gradient-to-r from-gradient-three-colorB to-gradient-three-colorA flex justify-center my-2 ">
         {" "}
         Welcome to SASI World
       </HeadingOne>
       <BodyOne className=" p-4 text-center">
-        I'm baby readymade thundercats offal, semiotics swag subway tile
-        actually jianbing. Enamel pin pok pok cliche live-edge disrupt put a
-        bird on it tote bag master cleanse subway tile heirloom cred kombucha
-        seitan kogi lo-fi. Raclette wolf celiac man braid, migas neutra
-        taxidermy twee chartreuse adaptogen kombucha put a bird on it. Vape
-        chicharrones pok pok try-hard humblebrag mumblecore. Waistcoat wayfarers
-        tofu, pork belly viral edison bulb raw denim selvage cred gochujang
-        mixtape cardigan distillery etsy leggings. Mixtape lomo readymade pok
-        pok gluten-free crucifix. Etsy blog lo-fi everyday carry tbh.
+        Finding the right visuals to use for your business can be a daunting
+        task! <br />
+        <br />
+        This is why we are creating SASI.
+        <br />
+        <br /> Content that is created to make your business stand out.
+        <br /> <br />
+        Get access to empowering imagery that promotes realness and celebrate
+        our unique differences.
+        <br />
+        <br />
+        Sign up and we'll notify you as soon as you can download some Free
+        Quality content! Get SASI imagery that speaks to your audience.
+        <br />
+        <br />
+        An easier way of marketing your product and business with relevant
+        content.
       </BodyOne>
 
       <div className=" p-3 flex justify-center">
         {socialLinks.map((link, index) => (
-          <SocialButton className="mx-3" key={index}>
+          <SocialButton className="mx-3 my-3" key={index}>
             <a className=" text-gray-800" href={link.url}>
               {link.icon}{" "}
             </a>
