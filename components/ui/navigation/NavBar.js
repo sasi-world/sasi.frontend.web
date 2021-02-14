51e1726c992be5b7937e0b365dcb5cb67a494817
Brandon <brandon.dlamini95@gmail.com>
2021-02-14 23:32:01 +0200

    added image to Navbar

--- components/ui/navigation/NavBar.js
+++ components/ui/navigation/NavBar.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Image from "next-images";
+import Logo from "./svg/Logo";
 
 const NavBar = () => {
   return (
     <header className="border-top bg-transparent flex items-center justify-between flex-wrap p-6">
       <div className="flex items-center flex-no-shrink text">
+        <Logo className=" mx-5" />
         <h1 className="text-white shadow-sm text-center font-bold font-display lg:text-3xl s:text-xl flex justify-center">
-          Sasi{" "}
+          SASI{" "}
         </h1>
       </div>
       {/* <span>
