8462ecd43617963c0c2a77fd875f7312b79a7e7b
Brandon <brandon.dlamini95@gmail.com>
2021-02-14 23:47:06 +0200

    adjusted navbar

--- components/ui/navigation/NavBar.js
+++ components/ui/navigation/NavBar.js
@@ -8,8 +8,8 @@ const NavBar = () => {
     <header className="border-top bg-transparent flex justify-center flex-wrap p-6">
       <div className="flex items-center flex-no-shrink text">
         <Logo className=" mx-2" />
-        <h1 className=" text-2xl text-white shadow-sm text-center font-bold font-display lg:text-3xl s:text-xl flex justify-center">
-          SASI{" "}
+        <h1 className=" text-2xl text-white shadow-sm text-center font-medium font-display lg:text-3xl s:text-xl flex justify-center">
+          SASI
         </h1>
       </div>
       {/* <span>
