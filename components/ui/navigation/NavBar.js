abe0dabb35022c2cdb81ae7cf88f4556f8c7a1fc
AmoDinho <u14284783@tuks.co.za>
2021-02-15 21:30:42 +0200

    fixing issues with images

--- components/ui/navigation/NavBar.js
+++ components/ui/navigation/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next-images";
+import Image from "next/image";
 import Logo from "../../../assets/sasi-logo.svg";
 import HeadingOne from "../Typography/HeadingOne";
 
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <header className="border-top bg-transparent flex justify-center flex-wrap p-6">
       <div className="flex items-center flex-no-shrink text">
-        <Image src={Logo} alt="Sasi logo" />
+        <Image src={Logo} alt="Sasi logo" height={100} width={40} />
         <HeadingOne className="  text-white  text-center">SASI</HeadingOne>
       </div>
       {/* <span>
