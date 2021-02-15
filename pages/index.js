75ed4f46c081ffa5b5f34cee6b160e6e5e5b979c
AmoDinho <u14284783@tuks.co.za>
2021-02-15 21:36:56 +0200

    done with copy

--- pages/index.js
+++ pages/index.js
@@ -9,42 +9,40 @@ import { socialLinks } from "../constants";
 const IndexPage = () => {
   return (
     <App>
-      <HeadingOne className="text-center  font-medium bg-clip-text my text-transparent bg-gradient-to-r from-gradient-three-colorB to-gradient-three-colorA flex justify-center mt-20 my-2 ">
+      <HeadingOne className="text-center  font-medium bg-clip-text my text-transparent bg-gradient-to-r from-gradient-three-colorB to-gradient-three-colorA flex justify-center mt-10 my-2 ">
         {" "}
         Welcome to SASI World
       </HeadingOne>
-      <div>
-        <BodyOne className=" mt-10 p-4 text-center">
+      <div className="flex flex-col text-center">
+        <BodyOne className=" mt-5">
           Finding the right visuals to use for your business can be a daunting
           task!
         </BodyOne>
 
-        <BodyOne className=" mt-10 p-4 text-center">
-          This is why we are creating SASI.
-        </BodyOne>
-        <BodyOne className=" mt-10 p-4 text-center">
+        <BodyOne className=" mt-5">This is why we are creating SASI.</BodyOne>
+        <BodyOne className=" mt-5">
           Content that is created to make your business stand out.
         </BodyOne>
-        <BodyOne className=" mt-10 p-4 text-center">
+        <BodyOne className=" mt-5">
           Finding the right visuals to use for your business can be a daunting
           task!
         </BodyOne>
 
-        <BodyOne>This is why we are creating SASI.</BodyOne>
+        <BodyOne className="mt-5">This is why we are creating SASI.</BodyOne>
 
-        <BodyOne>
+        <BodyOne className="mt-5">
           Content that is created to make your business stand out.
         </BodyOne>
 
-        <BodyOne>
+        <BodyOne className="mt-5">
           Get access to empowering imagery that promotes realness and celebrate
           our unique differences.
         </BodyOne>
-        <BodyOne>
+        <BodyOne className="mt-5">
           Sign up and we'll notify you as soon as you can download some Free
           Quality content! Get SASI imagery that speaks to your audience.
         </BodyOne>
-        <BodyOne>
+        <BodyOne className="mt-5">
           An easier way of marketing your product and business with relevant
           content.
         </BodyOne>
