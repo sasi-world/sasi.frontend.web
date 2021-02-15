395ec25d0c5d80e8295a7180115c5d17393c5d2c
AmoDinho <u14284783@tuks.co.za>
2021-02-15 21:27:35 +0200

    imported sasi logog

--- components/ui/navigation/NavBar.js
+++ components/ui/navigation/NavBar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Image from "next-images";
-import Logo from ".";
+import Logo from "../../../assets/sasi-logo.svg";
 import HeadingOne from "../Typography/HeadingOne";
 
 const NavBar = () => {
   return (
     <header className="border-top bg-transparent flex justify-center flex-wrap p-6">
       <div className="flex items-center flex-no-shrink text">
-        <Logo className=" mx-2" />
+        <Image src={Logo} alt="Sasi logo" />
         <HeadingOne className="  text-white  text-center">SASI</HeadingOne>
       </div>
       {/* <span>
